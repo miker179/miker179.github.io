563e1a944646a5fdf07a00d1722d3bcca2a358ec
miker179 <mike.rivera179@gmail.com>
2020-04-02 20:04:36 -0400

    reuploading this and try to make the development branch

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './MichaelRIvera.JPG';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          My name is Michael Rivera and Im going to master React!!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/miker179"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Check Me Out !!!!!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
