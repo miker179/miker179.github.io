f83043cb5de5a53ada49ed6eb368ff7e7c0c527c
miker179 <mike.rivera179@gmail.com>
2020-05-25 02:20:43 -0400

    Try to do the toggle nav but cant make it to work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,28 +8,55 @@ function Navbar() {
         //     <Link to="/projects">Projects </Link>
         //     <Link to="/aboutme">About Me</Link>
         //   </div>
-        <div class="bg-dark row navi">
+        <div className="bg-dark row navi">
             <div class="col-2">
                 <a class="navbar-brand" href="#">MR</a>
             </div>
             <div class="col-4 offset-6">
-            <ul className="nav justify-content-center">
-                <li className="nav-item">
-                    <Link to="/">Home </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/projects">Projects </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/aboutme">About Me</Link>
-                </li>
-            </ul>
+                <ul className="nav justify-content-center dropdown">
+
+                    <li className="nav-item">
+                        <Link to="/">Home </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/projects">Projects </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/aboutme">About Me</Link>
+                    </li>
+
+                </ul>
+                <div className="burger">
+                    <div clasName="line1"></div>
+                    <div clasName="line2"></div>
+                    <div clasName="line3"></div>
+                </div>
             </div>
-            
         </div>
-        
+
 
     );
 };
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.dropdown');
+    const navLinks = document.querySelectorAll('.navi li');
+
+    //toggle nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('navi-active');
+    });
+    
+    //Animate links
+    navLinks.forEach((link, index) =>{
+        link.style.animation = 'navLinkFade 0.5s ease forwards ${ index / 7';
+    });
+
+}
+
+window.onload = function () {
+    navSlide();
+}
 
 export default Navbar;
