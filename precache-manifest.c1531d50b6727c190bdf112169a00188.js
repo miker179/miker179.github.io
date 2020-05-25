012b8045bc7c8493efe906b2ac2645a03e92d6de
miker179 <mike.rivera179@gmail.com>
2020-05-25 02:48:22 -0400

    Updates

---
+++ precache-manifest.c1531d50b6727c190bdf112169a00188.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd140959ec9b093169e6551075d2cc8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7a6c36415133ecd8053",
+    "url": "/static/css/main.bf9f2070.chunk.css"
+  },
+  {
+    "revision": "11a36f482675b1c7944c",
+    "url": "/static/js/2.90b77b09.chunk.js"
+  },
+  {
+    "revision": "bf37c44199e4ed51a0f13bad10eb1ca6",
+    "url": "/static/js/2.90b77b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a6c36415133ecd8053",
+    "url": "/static/js/main.b8cc8750.chunk.js"
+  },
+  {
+    "revision": "3cf377d5cb0fadb3f9a6",
+    "url": "/static/js/runtime-main.e5cedecd.js"
+  },
+  {
+    "revision": "f54e5b8e82915b7ab7b7bbccaa87d854",
+    "url": "/static/media/MichaelRIvera.f54e5b8e.JPG"
+  },
+  {
+    "revision": "c569c5d2d6d8763c1afc51ca0869487c",
+    "url": "/static/media/background.c569c5d2.jpg"
+  },
+  {
+    "revision": "3db5f5f677582f379661a060d3cb6e48",
+    "url": "/static/media/desktop.3db5f5f6.png"
+  },
+  {
+    "revision": "fab28003a1ab5e040fb98261d95705e3",
+    "url": "/static/media/mobile.fab28003.png"
+  },
+  {
+    "revision": "423d8a6493bef75a8c0ff2579fadc2eb",
+    "url": "/static/media/poke-jumbo.423d8a64.png"
+  },
+  {
+    "revision": "5c7845c5cf1d18c8f8fa4dd7b23adf05",
+    "url": "/static/media/pokemon-mobileWireframe.5c7845c5.png"
+  },
+  {
+    "revision": "995318ae5eb71d04be0afacebbd62144",
+    "url": "/static/media/pokemon-wireframe.995318ae.png"
+  },
+  {
+    "revision": "63d9c23e3f0e69e31c22e41289c2f2a3",
+    "url": "/static/media/pokemonWB.63d9c23e.png"
+  }
+]);
